55b42d0d9f0b23b719a4abc24c62d66d9c4b275a
cale-i <calei078029@gmail.com>
2021-10-28 18:28:59 +0900

    Add flux tutorial

---
+++ 3-flux/readme.md
@@ -0,0 +1,9 @@
+# Setup
+
+npm init -y
+npm install --save-dev @babel/core @babel/preset-env @babel/preset-react babel-loader \
+ babel-plugin-react-html-attrs babel-plugin-add-module-exports \
+ babel-plugin-transform-class-properties babel-plugin-transform-decorators-legacy \
+
+npm install --save-dev react react-dom react-router react-router-dom webpack webpack-cli webpack-dev-server
+npm install --save-dev flux
---
+++ 3-flux/src/js/pages/Todos.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+import Todo from "../components/Todo";
+import * as TodoActions from "../actions/TodoActions";
+import TodoStore from "../stores/TodoStore";
+
+export default class Todos extends React.Component {
+	constructor() {
+		super();
+		this.getTodos = this.getTodos.bind(this);
+		this.state = {
+			todos: TodoStore.getAll(),
+		};
+	}
+	componentDidMount() {
+		// Vue.jsの"mounted"と似た機能
+		TodoStore.on("change", this.getTodos);
+		console.log("count", TodoStore.listenerCount("change"));
+	}
+	componentWillUnmount() {
+		TodoStore.removeListener("change", this.getTodos);
+	}
+	getTodos() {
+		this.setState({
+			todos: TodoStore.getAll(),
+		});
+	}
+	createTodo() {
+		TodoActions.createTodo("New Todo");
+	}
+	reloadTodos() {
+		TodoActions.reloadTodos();
+	}
+	render() {
+		const { todos } = this.state;
+
+		const TodoComponents = todos.map((todo) => {
+			return <Todo key={todo.id} {...todo} />;
+		});
+
+		return (
+			<div>
+				<button onClick={this.reloadTodos.bind(this)}>Reload!</button>
+				<h1>Todos</h1>
+				<ul>{TodoComponents}</ul>
+			</div>
+		);
+	}
+}
